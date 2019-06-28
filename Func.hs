8d145cac222bc7ae0ed4b63c9152003da5fdaeb3
Otavio Cozer <otaviocozer22@gmail.com>
2019-06-28 13:10:09 -0300

    Árvore e Classificação possivelmente corretas. Porém, muito provável que haja espaços para muita otimização.

--- Func.hs
+++ Func.hs
@@ -2,7 +2,9 @@ module Func(
  melhorTeste,
  percentagens,
  ehValorNumerico,
- definition1
+ definition1,
+ compararCaso,
+ refinarBase
 )where
 
 import Debug.Trace
@@ -27,16 +29,17 @@ entropia' (ocorrencia:ocorrencias) = - (ocorrencia * (lg ocorrencia)) + entropia
 
 percentagens ex (classe:classes) = [    (frequency x ex)/(tamanho ex)     | x <- classes ] 
 
+frequency classe ([]:_) = 0
 frequency classe [] = 0
-frequency classe (ex:exs) = if (last ex) == classe then 1 + frequency classe exs else 0 + frequency classe exs 
-
+frequency classe (ex:exs) = if (last ex) == classe then 1 + frequency classe exs else 0 + frequency classe exs --Essa classe está errada???? tenho que avaliar melhor depoisss
+                                                                                                               -- em uma das chamas (ex:exs) = [[][][][][]] isso deveria aoontecer??
 
 -------------------------------------------------------------------------------------------------------------------
 
 -- a funcção a seguir implementa a seguinte definição {x E Ex/ values(x,a)=v} mostrada no enunciado do trabalho--
 -- ela deve passar a sua base de dados e um valor de uma dada caracteristica
-
-definition1 ex value | (cabeca == "<=" || cabeca == "<>" || cabeca == ">>")  = definitionNumerico ex (words value)
+definition1 ([]:_) _ = []
+definition1 ex value | (cabeca == "<=" || cabeca == "<>" || cabeca == ">>")  = (definitionNumerico ex (words value))--Essa classe está errada???? tenho que avaliar melhor depoisss -- em uma das chamas (ex:exs) = [[][][][][]] isso deveria aoontecer??
                      | otherwise = definitionString ex value
                         where cabeca = head (words value)
 
@@ -91,9 +94,14 @@ igrLista _ [] _ = []
 igrLista ex (a:as) classes = (igr ex a classes) : (igrLista (map tail ex) as classes)    
 
 --melhor Teste retorna o nome do melhor teste e o seu indice no grupo de caractersiticas--
-melhorTeste ex a classes = retornaMaior igrs a
+melhorTeste ex a classes fixedChar = newTuple
                             where igrs = igrLista ex a classes
+                                  tuple = retornaMaior igrs a
+                                  index = retira (elemIndex (head (fst tuple)) fixedChar)
+                                  newTuple = insere tuple index
 
+retira (Just x) = x
+insere (x,y) z = (x,y,z) 
 --retorna o maior elemento e um indice--
 retornaMaior igrs a = retornaMaior' igrs a ((head a),0) 0 (head igrs)
 
@@ -108,6 +116,31 @@ ehValorNumerico'  parseValue | head parseValue == "<=" = True
                                       | head parseValue == "<>" = True 
                                       | head parseValue == ">>" = True
                                       | otherwise = False
+
+--A fincao abaixo determina se um dado caso possui um dado valor de uma caracteristica--
+compararCaso valueCaso valueCaracteristica | (cabeca == "<=" || cabeca == "<>" || cabeca == ">>")  = compararCasoNumerico  valueCaso (words valueCaracteristica)
+                                           | otherwise = valueCaso == valueCaracteristica 
+                                             where cabeca = head (words valueCaracteristica)
+
+compararCasoNumerico valueCaso parseValue | head parseValue == "<=" = valueCaso <= (parseValue!!1)  
+                                          | head parseValue == "<>" = (valueCaso > (parseValue!!1)) && (valueCaso <= (parseValue!!2)) 
+                                          | head parseValue == ">>" = valueCaso > (parseValue!!1)  
+
+
+
+
+----
+--refinarBase ([]:_) _ = []
+refinarBase ex value index | (cabeca == "<=" || cabeca == "<>" || cabeca == ">>")  = (refinarBaseNumerico ex (words value) index)--Essa classe está errada???? tenho que avaliar melhor depoisss -- em uma das chamas (ex:exs) = [[][][][][]] isso deveria aoontecer??
+                           | otherwise = refinarBaseString ex value index
+                              where cabeca = head (words value)
+
+refinarBaseNumerico ex parseValue index | head parseValue == "<=" = [ x | x <- ex, (x!!index) <= (parseValue!!1)  ] 
+                                        | head parseValue == "<>" = [ x | x <- ex, ((x!!index) > (parseValue!!1)) && ((x!!index) <= (parseValue!!2)) ] 
+                                        | head parseValue == ">>" = [ x | x <- ex, (x!!index) > (parseValue!!1)  ] 
+
+
+refinarBaseString ex value index = [ x | x <- ex, (x!!index) == value ] 
 
 caracteristicas1 = [["Aparencia","Sol","Chuva","Nublado"],["Temperatura","<= 22.5","<> 22.5 24.0","<> 24.0 26.5",">> 26.5"],["Umidade","<= 78.5",">> 78.5"],["Vento","Sim","Nao"]]
 
--- Main.hs
+++ Main.hs
@@ -1,37 +1,57 @@
 import Leitura
 import Func
 import Discretization
+import Debug.Trace
 
-data Arv =   No {pergunta :: String, filhos :: [Arv], resposta :: String} 
+data Arv =   No {pergunta :: String, filhos :: [Arv], resposta :: String, index :: Int} 
           | Folha{classe :: String, resposta :: String} 
           | Nil deriving Show
 
 
 main = do
-            nome <- readFile "base.txt"
-            putStrLn  $ show (leEntrada nome)
+            baseIO <- readFile "base1.txt"
+            caracteristicasIO <- readFile "descricao1.txt"
+            casoIO <- readFile "caso1.txt"
+            let base = leEntrada baseIO
+            let caracteristicas' = leEntrada caracteristicasIO
+            let caracteristicas = insereValoresNumericos base (init caracteristicas') 
+            let classes = last caracteristicas'
+            let casos = leEntrada casoIO
+            let fixedChar = map head caracteristicas
+            let arvore = arvoreDecisao base caracteristicas (maioria base classes) classes "" fixedChar
+            let classified = classificarTodos casos arvore
+            putStrLn $ show base
+            --putStrLn $ show ( init caracteristicas')           
+            --putStrLn $ show caracteristicas
+            --putStrLn $ show (maioria base classes)
+            --putStrLn $ show classes 
+            --putStrLn $ show casos
+            writeFile "arv.txt" (show arvore)            --putStrLn (show classified)
+            writeFile "result.txt" (show classified)
+            putStrLn ("OKKK")
 
 
 --implementa o algoritmo da árvore de decisão--
 
 
-arvoreDecisao exemplos caracteristicas maisComum classes value | exemplos == [] = Folha{classe = maisComum, resposta = value} 
-                                                               | mesmaClasse exemplos = Folha{classe = (last (head exemplos)), resposta = value}
-                                                               | caracteristicas == [] = Folha{classe = maioria exemplos classes, resposta = value} 
-                                                               | otherwise = criaNo exemplos caracteristicas maisComum classes value
+arvoreDecisao exemplos caracteristicas maisComum classes value fixedChar | exemplos == [] = Folha{classe = maisComum, resposta = value} 
+                                                                         | mesmaClasse exemplos = Folha{classe = (last (head exemplos)), resposta = value}
+                                                                         | caracteristicas == [] = Folha{classe = maioria exemplos classes, resposta = value} 
+                                                                         | otherwise = criaNo exemplos caracteristicas maisComum classes value fixedChar
 
-criaNo exemplos caracteristicas maisComum classes value = forEachValue arvore exemplos newCaracteristicas values maisComum classes newExemplos
-                                                        where melhor = melhorTeste exemplos caracteristicas classes
-                                                              melhorChar = fst melhor
-                                                              melhorIndex = snd melhor
-                                                              values = tail (melhorChar)
-                                                              arvore = raiz (head melhorChar) value
-                                                              newCaracteristicas = filter (/=melhorChar) caracteristicas
-                                                              newExemplos = map (drop melhorIndex) exemplos
+criaNo exemplos caracteristicas maisComum classes value fixedChar = (forEachValue arvore exemplos newCaracteristicas values maisComum classes newExemplos melhorIndex fixedChar)
+                                                               where melhor = melhorTeste exemplos caracteristicas classes fixedChar
+                                                                     melhorChar = fst' melhor
+                                                                     melhorIndex = snd' melhor
+                                                                     arvIndex = trd melhor
+                                                                     values = tail (melhorChar)
+                                                                     arvore = raiz (head melhorChar) value arvIndex
+                                                                     newCaracteristicas = filter (/=melhorChar) caracteristicas
+                                                                     newExemplos = map (drop melhorIndex) exemplos
 
 -- As funções mesmaClasse e mesmaClasse' são usadas para verificar se um conjunto de dados possui todas as classes iguais
 mesmaClasse :: [[String]] -> Bool
-mesmaClasse (x:xs) = mesmaClasse' (last x) (x:xs) True             
+mesmaClasse (x:xs) =  mesmaClasse' (last x) (x:xs) True             
 
 mesmaClasse' :: String -> [[String]] -> Bool -> Bool
 mesmaClasse' _ [] True = True
@@ -39,17 +59,18 @@ mesmaClasse' anterior (x:xs) igual  = if classe == anterior then mesmaClasse' cl
                                     where classe =  last x
 
 
-raiz melhor value = No {pergunta = melhor, filhos = [], resposta = value}
+raiz melhor value indice = No {pergunta = melhor, filhos = [], resposta = value, index=indice}
 
-forEachValue arvore _ _ [] _ _ _ = arvore 
-forEachValue arvore exemplos caracteristicas (value:values) maisComum classes newExemplos = forEachValue newArvore exemplos caracteristicas values maisComum classes newExemplos
-                                                                             where exemplosi = definition1 newExemplos value --toda hora chamar map drop index é burro. tem q ajeita isso 
-                                                                                   subArvore = arvoreDecisao exemplosi caracteristicas (maioria exemplos classes) classes value 
+forEachValue arvore _ _ [] _ _ _ _ _ = arvore 
+forEachValue arvore exemplos caracteristicas (value:values) maisComum classes newExemplos index fixedChar = forEachValue newArvore exemplos caracteristicas values maisComum classes newExemplos index fixedChar
+                                                                             where exemplosi = refinarBase exemplos value index --toda hora chamar map drop index é burro. tem q ajeita isso 
+                                                                                   subArvore = {- trace("\n--Calling exemplosi=" ++ (show exemplosi) ++ " And exemplosatualizados=" ++ (show exemplosAtualizados) ++ " And caracteristicas=" ++ (show caracteristicas) ) -}(arvoreDecisao exemplosAtualizados caracteristicas (maioria exemplos classes) classes value fixedChar) 
                                                                                    newArvore = adicionaFilho arvore subArvore
+                                                                                   exemplosAtualizados = map (removeItem index) exemplosi
 {- refinandoBase exemplos value | ehValorNumerico value = refinandoBaseNumerico exemplos value
                              | otherwise = refinandoBaseNumerico exemplos value -}
 
-adicionaFilho (No a filhos b) subArvore = (No a (subArvore : filhos) b)
+adicionaFilho (No a filhos b i) subArvore = (No a (subArvore : filhos) b i)
 
 melhorTeste1 ex a classes = (["Umidade","<= 78.5",">> 78.5"],2)
 
@@ -60,12 +81,29 @@ maioria ex classes = maioria' ocorrencias (tail classes) (maximum ocorrencias)
 
 maioria' (o:os) (c:cs) maior = if maior == o then c else maioria' os cs maior
 
+------------
+classificarTodos cs arv = [ classificar x arv | x <- cs ]
+
+classificar c (Folha classe' _) = classe'
+classificar c (No b (a:as) d i) = if compararCaso (c!!i) value then classificar c a  else classificar c (No b as d i) 
+                                          where value = resposta a
+                             
+
+removeItem 0 (y:ys) = ys
+removeItem n (y:ys) = y : removeItem (n-1) ys
+
+
+arv1 = No {pergunta = "Umidade", filhos = [Folha {classe = "NaoVa", resposta = ">> 78.5"},Folha {classe = "Va", resposta = "<= 78.5"}], resposta = "", index = 2}
+
+casos1 = ["Chuva","23","92","Sim"]
 
 moreCommom= "NaoVa"
 
 teste = ">> 78.5"
 
-
+trd (_,_,x) = x
+fst' (x,_,_) = x
+snd' (_,x,_) = x
 
 caracteristicas1 = [["Aparencia","Sol","Chuva","Nublado"],["Temperatura","<= 22.5","<> 22.5 24.0","<> 24.0 26.5",">> 26.5"],["Umidade","<= 78.5",">> 78.5"],["Vento","Sim","Nao"]]
 
@@ -82,6 +120,6 @@ ex2 = [["Sol","28","91","Sim","NaoVa"],
        ["Sol","30","85","Nao","NaoVa"]] 
 
 
-test1 = No {pergunta = "Umidade", filhos = [], resposta = ""}
+test1 = No {pergunta = "Umidade", filhos = [], resposta = "", index=1}
 
-test2 = No {pergunta = "oiee", filhos = [], resposta = ""}
+test2 = No {pergunta = "oiee", filhos = [], resposta = "", index=2}
